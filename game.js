541f44c293d89b3598fb46bd7f6b50fd7d99f64e
LazareBoukenouche <lazare.boukenouche@gmail.com>
2018-12-26 03:00:30 +0100

    Replaced if-else structure in drawAllBlocks() function to switch-case structure. Modified checkBlockColision() function, to deplace individual block when a collision happens.

--- game.js
+++ game.js
@@ -4,15 +4,10 @@ const LEFT = 0;
 const BLACK = "#000000";
 const WHITE = "#FFFFFF";
 const RED = "#FF0000";
-const DARKRED = "#AA0000";
 const GREEN = "#00FF00";
 const BLUE = "#0000FF";
 const YELLOW = "#FFFF00";
 const ORANGE = "#FF7000";
-
-// Declaration of the width and height of the player
-const HEIGHT_PLAYER = 10;
-const WIDTH_PLAYER = 190;
 
 // Create the canvas window, where the game will be played.
 let canvas = document.getElementById('canvas');
@@ -20,37 +15,47 @@ let context = canvas.getContext('2d');
 let startingCount = 1;
 let timeInterval = 10;
 
+// Declaration of the width and height of the player
+const HEIGHT_PLAYER = 10;
+const WIDTH_PLAYER = canvas.width/2;
+
 // Declaration of the coordinates and the speed of the player
 let initialPlayerYpos = 400;
 let playerXpos = 240;
 let playerSpeed = -30;
+let playerCollision = true;
 
 // Declaration of the coordinates and the speed of the ball
 let circleXPos = 200;
 let circleYPos = 275;
 let circleRadius = 10;
-let circleSpeedX = 5;
-let circleSpeedY = 5;
+let circleSpeedX = 25;
+let circleSpeedY = 25;
 
+// Declaration of the variables of the blocks
 let numberBlockCollision = 3;
 let blockWidth = 60;
 let blockHeight = 20;
 let firstBlockCoordX = LEFT;
 let firstBlockCoordY = TOP + 10;
-let rows = 2;
-let columns = Math.floor(Math.random() * (7 - 1)) + 1;
+let allBlocks = [
+                    [{x:75*0,y:22.91*1},{x:75*0,y:22.91*2},{x:75*0,y:22.91*3}],
+                    [{x:75*1,y:22.91*1},{x:75*1,y:22.91*2},{x:75*1,y:22.91*3}],
+                    [{x:75*2,y:22.91*1},{x:75*2,y:22.91*2},{x:75*2,y:22.91*3}],
+                    [{x:75*3,y:22.91*1},{x:75*3,y:22.91*2},{x:75*3,y:22.91*3}],
+                    [{x:75*4,y:22.91*1},{x:75*4,y:22.91*2},{x:75*4,y:22.91*3}],
+                    [{x:75*5,y:22.91*1},{x:75*5,y:22.91*2},{x:75*5,y:22.91*3}],
+                    [{x:75*6,y:22.91*1},{x:75*6,y:22.91*2},{x:75*6,y:22.91*3}],
+                    [{x:75*7,y:22.91*1},{x:75*7,y:22.91*2},{x:75*7,y:22.91*3}],
+                ];
 
 // Main Game loop, with an interval of time implemented with a setTimeout function
 function main() {
-	setTimeout(function onTick(timeInterval) {
-    //Drawing the canvas window
-    clearCanvas(LEFT,TOP,canvas.width,canvas.height);
-    checkAllCollision();
-    drawAllBlocks();
     startingCount = 0;
-    setColors(BLACK,GREEN);
-    drawRect(playerXpos,initialPlayerYpos,WIDTH_PLAYER,HEIGHT_PLAYER);
-    drawCircle(circleXPos,circleYPos,circleRadius,0,2*Math.PI);
+	setTimeout(function onTick(timeInterval) {
+    clearCanvas(LEFT,TOP,canvas.width,canvas.height);
+    drawAllGameElements();
+    checkAllCollision();
     moveCircle(circleSpeedX, circleSpeedY);
     // Call main again
     main();
@@ -90,12 +95,15 @@ function setColors(colorBorder,colorFull){
     }
 
 ////////// DRAWING FUNCTIONS  //////////
+
+// Draw the player cursor
 function drawRect(coordX,coordY,width,height){
     // fillRect() draw a full rectangle and strokeRect() draw a border
     context.fillRect(coordX,coordY,width,height);
     context.strokeRect(coordX,coordY,width,height);
     }
 
+// Draw the ball
 function drawCircle(xPos,yPos,radius,startingAngle,endingAngle) {
     setColors(BLUE,BLUE);
     context.beginPath();
@@ -103,45 +111,38 @@ function drawCircle(xPos,yPos,radius,startingAngle,endingAngle) {
     context.fill();
 }
 
+// Draw all the blocks
 function drawAllBlocks() {
     // Draw all the blocks, vertically first and then horizontally
-    for (var x = 0; x < canvas.width;x+= (canvas.width/12)+15){
-        for (var y = 0; y < canvas.height/3;y+= (canvas.height/24)+5){
+    for (var x = 0; x < allBlocks.length; x++){
+        for (var y = 0; y < allBlocks[0].length; y++){
             // Check how often the block has collide with the ball 
             // and change the drawing colors accordingly, then draw the block
-            if (numberBlockCollision === 3) {
-                setColors(BLACK,GREEN);
-                drawRect(x,y,blockWidth,blockHeight);
-            }
-            else if (numberBlockCollision === 2) {
-                setColors(BLACK,YELLOW);
-                drawRect(x,y,blockWidth,blockHeight);
-            }
-            else if (numberBlockCollision === 1.5) {
-                setColors(BLACK,ORANGE);
-                drawRect(x,y,blockWidth,blockHeight);
-            }
-            else if (numberBlockCollision === 0.5) {
-                setColors(BLACK,RED);
-                drawRect(x,y,blockWidth,blockHeight);
-            }
-            else if (numberBlockCollision <= 0) {
-                setColors(WHITE,WHITE);
-                drawRect(x,y,blockWidth,blockHeight);
+            switch (numberBlockCollision) {
+                case 3:
+                    setColors(BLACK,GREEN);
+                    drawRect(allBlocks[x][y].x,allBlocks[x][y].y,blockWidth,blockHeight);
+                    break;
+                default:
+                    setColors(BLACK,BLACK);
+                    drawRect(allBlocks[x][y].x,allBlocks[x][y].y,blockWidth,blockHeight);
+                    break;
+                    }
+                }
             }
         }
-    }
-}
 
+// Regroup all the drawing functions under this one
+function drawAllGameElements() {
+    drawAllBlocks();
+    drawRect(playerXpos,initialPlayerYpos,WIDTH_PLAYER,HEIGHT_PLAYER);
+    drawCircle(circleXPos,circleYPos,circleRadius,0,2*Math.PI);
+}
 ////////// END DRAWING FUNCTIONS  //////////
 
 ////////// COLLISION FUNCTIONS  //////////
-function checkAllCollision() {
-    checkWallCollision();
-    checkBlockCollision();
-    checkPlayerCollision();
-}
 
+// Check if the ball hit a wall
 function checkWallCollision() {
     if (circleXPos === 0) {
         circleSpeedX = -circleSpeedX;
@@ -157,35 +158,53 @@ function checkWallCollision() {
     }
 }
 
+// Check if the ball hit the cursor
 function checkPlayerCollision() {
     if (playerXpos < circleXPos + circleRadius &&
         playerXpos + WIDTH_PLAYER > circleXPos &&
         initialPlayerYpos < circleYPos + circleRadius &&
-        HEIGHT_PLAYER + initialPlayerYpos > circleYPos){
-            circleSpeedY = 10;
+        HEIGHT_PLAYER + initialPlayerYpos > circleYPos && playerCollision === true){
+            circleSpeedY = -5;
+            circleSpeedX = -5;
+            playerCollision = false;
+        }
+        else if (playerXpos < circleXPos + circleRadius &&
+        playerXpos + WIDTH_PLAYER > circleXPos &&
+        initialPlayerYpos < circleYPos + circleRadius &&
+        HEIGHT_PLAYER + initialPlayerYpos > circleYPos && playerCollision === false){
+            circleSpeedY = -5;
             circleSpeedX = 5;
+            playerCollision = true;
         }
     }
-
+    
+// Check if the ball hit on of the blocks
 function checkBlockCollision() {
-
-    for (var x = 0; x < canvas.width;x+= (canvas.width/12)+5){
-        for (var y = 0; y < canvas.height/3;y+= (canvas.height/24)+2){
-            if (x < circleXPos + circleRadius &&
-            x + blockWidth > circleXPos &&
-            y < circleYPos + circleRadius &&
+    for (var x = 0; x < allBlocks.length; x++){
+        for (var y = 0; y < allBlocks[x].length; y++){
+            if (allBlocks[x][y].x < circleXPos + circleRadius &&
+            allBlocks[x][y].x + blockWidth > circleXPos &&
+            allBlocks[x][y].y < circleYPos + circleRadius &&
             blockHeight + y > circleYPos && numberBlockCollision > 0){
                 circleSpeedY = 10;
                 circleSpeedX = 5;
-                drawRect(x,y,blockWidth, blockHeight);
-                numberBlockCollision -= 0.5;
+                allBlocks[x][y].y = -30;
+                allBlocks[x][y].x = -300;
             }
         }
-        
     }
 }
+
+// Regroup all the collison functions under this one
+function checkAllCollision() {
+    checkWallCollision();
+    checkBlockCollision();
+    checkPlayerCollision();
+}
+
 ////////// END COLLISION FUNCTIONS  //////////
 
+////////// DEPLACEMENT FUNCTIONS  //////////
 function movePlayer(speed) {
     playerXpos = playerXpos - speed;
 }
@@ -194,7 +213,7 @@ function moveCircle(speedX, speedY) {
     circleXPos += speedX;
     circleYPos += speedY;
 }
-
+////////// END DEPLACEMENT FUNCTIONS  //////////
 
 // Creating keyboard events with the Char codes of the keyboard
 document.onkeydown = function(e) {
@@ -209,29 +228,40 @@ document.onkeydown = function(e) {
             break;
         // Pressing L
         case 76:
+            console.log(allBlocks);
             break;
         // Pressing S
         case 83:
             numberBlockCollision -= 1;
             break;
 
-    // Keyboard keys for changing the direction of the snake
+    // Keyboard keys for changing the direction of the player
 
         //Left arrow
         case 37:
-            movePlayer(35);
+            if (playerXpos > 0) {
+            movePlayer(165);
+            }
+            else {
+                movePlayer(0);
+            }
             break;
         //Up arrow
         case 38:
             break;
         //Right arrow
         case 39:
-            movePlayer(-35);
+            if (playerXpos <= canvas.width - WIDTH_PLAYER) {
+            movePlayer(-165);
+            }
+            else {
+                movePlayer(0);
+            }
             break;
         //Down arrow
         case 40:
     }
 };
 
-// Main game loop
+// Launch the game
 main()
